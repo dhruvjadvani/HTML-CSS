bc20f93c585674ca4ac381c92de17aacfbfe8d49
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-06-26 18:05:38 +0530

    :)

--- transitions/index.js
+++ transitions/index.js
@@ -1,1 +1,6 @@
+const el = document.getElementById("app");
 
+window.addEventListener("click", function (event) {
+    const{clientX, clientY}= event;
+    el.style.transform = `translate(${clientX - 50}px, ${clientY - 50}px)`
+});
