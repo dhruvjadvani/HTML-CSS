a71ae141e7004b9fb1f429618603ed2234a88bf9
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-10 17:56:44 +0530

    :)

--- JS Intro/isPrime.js
+++ JS Intro/isPrime.js
@@ -1,1 +1,15 @@
+let number = 23;
+let flag = true;
 
+for(let div = 2; div < number; div++) {
+    if(number % div == 0) {
+        flag = false;
+        break;
+    }
+}
+
+if(flag == true) {
+    console.log(number, " is prime");
+} else {
+    console.log(number, " is not prime");
+}
