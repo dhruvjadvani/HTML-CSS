693af883011303140d42399838e50bd1a67c14ef
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-10 17:20:09 +0530

    :)

--- JS Intro/intro.js
+++ JS Intro/intro.js
@@ -1,1 +1,18 @@
+console.log("Hello JS :)");
+// variable declare
+let a;
+a = 10;
+a = 10.1;
+
+console.log("variable contains", a);
+
+a = "Hello I am a string";
+a = 'Hello I am also a string';
+
+a = null;
+a = true;
+
+console.log("variable contains", a);
+
+
 
