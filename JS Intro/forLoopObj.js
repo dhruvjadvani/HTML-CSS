446c798590a108d365146654731e79ecd8b462c9
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-10 17:26:24 +0530

    :)

--- JS Intro/forLoopObj.js
+++ JS Intro/forLoopObj.js
@@ -1,1 +1,20 @@
+let person = {
+    name: "Dhruv",
+    lastName: "Jadvani",
+    address: {
+        city: "Ahmedabad",
+        state: "Gujarat"
+    } ,
+    age: 23,
+    isEngineer: true,
+    companies: ["Keepsake", "TCS"],
+    sayHi: function () {
+        console.log("Hi!");
+    } 
+};
+
+for(let key in person) {
+console.log(key, " : ", person[key]);
+}
+
 
